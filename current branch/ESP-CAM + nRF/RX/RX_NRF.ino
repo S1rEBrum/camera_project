18dd85084e49ab7cb97faa23b8c06872c1fd0d9d
S1rEBrum <85932316+S1rEBrum@users.noreply.github.com>
2024-07-25 10:43:42 +0300

    Add files via upload

--- current branch/ESP-CAM + nRF/RX/RX_NRF.ino
+++ current branch/ESP-CAM + nRF/RX/RX_NRF.ino
@@ -1,66 +1,30 @@
-#include "multiple_spi.h"
-
-#include <nRF24L01.h>
-#include <RF24.h>
-#include <SPI.h>
-#include <SD.h>
-
-#define BAUDRATE 115200
-#define SET_DEBUG false
-#define PACKET_SIZE 30
-#define PIPE 0x1234567890LL
-
-uint8_t *fb = nullptr;
-uint8_t buf[PACKET_SIZE] = { 0 };
-int image_size = 0;
-int packet_counter = 0;
-int image_counter = 0;
-
-bool receiving_image = false;
-bool receiving_credentials = false;
-
-RF24 radio(4, 15);
+//---------------------------------------
+// RX MODULE
+//---------------------------------------
+#include "routins.h"  // Include the routines header file
 
 void setup() {
-  //initialize two instances of the SPIClass attached to VSPI and HSPI respectively
-  vspi = new SPIClass(VSPI);
-  hspi = new SPIClass(HSPI);
+  // Initialize multiple SPI interfaces
+  init_spi();
+  delay(1000);  // Delay to allow for proper initialization
 
-  vspi->begin();
-  hspi->begin();
-
-  vspi->begin(VSPI_SCLK, VSPI_MISO, VSPI_MOSI, VSPI_SS);  //SCLK, MISO, MOSI, SS
-  hspi->begin(HSPI_SCLK, HSPI_MISO, HSPI_MOSI, HSPI_SS);  //SCLK, MISO, MOSI, SS
-
-  pinMode(vspi->pinSS(), OUTPUT);  //VSPI SS
-  pinMode(hspi->pinSS(), OUTPUT);  //HSPI SS
-
-  delay(1000);
+  // Begin serial communication at the defined baud rate
   Serial.begin(BAUDRATE);
 
-  // radio setup
-  if (!radio.begin(hspi)) {
-    Serial.println("Could not initialize radio");
-    return;
-  } else {
-    Serial.println("Radio initialized successfully");
-  }
-  radio.setChannel(5);
-  radio.setDataRate(RF24_1MBPS);
-  radio.setPALevel(RF24_PA_HIGH);
-  radio.openReadingPipe(1, PIPE);
-  radio.startListening();
+  // Initialize the RF module
+  init_radio();
 
-  // SD card setup
+  // Initialize the microSD card module
   init_sd();
 }
 
 void loop() {
-  // if (radio.available()) {
-  //   radio.read(&weight, sizeof(weight));
-  // }
+  // Check if there is data available to read from the radio
   if (radio.available()) {
+    // Read the data into the buffer
     radio.read(buf, PACKET_SIZE);
+
+    // If debugging is enabled, print the packet contents
     if (SET_DEBUG) {
       Serial.printf("Packet %d: ", packet_counter);
       for (int i = 0; i < PACKET_SIZE; ++i) {
@@ -69,15 +33,20 @@ void loop() {
       Serial.println();
     }
 
+    // Check if the end of the image has been received
     if (got_image_end(buf)) {
-      Serial.printf("Got end packet, before saving image, the size is %d\n", image_size);
+      Serial.println("Got end packet");
+      // Save the image to the SD card
       save_image_to_sd(fb, image_size);
-      delete[] fb;
-      image_size = 0;
-      packet_counter = 0;
-      receiving_image = false;
-      receiving_credentials = false;
-    } else if (receiving_image) {
+      delete[] fb;                    // Free the allocated memory for the image
+      image_size = 0;                 // Reset the image size
+      packet_counter = 0;             // Reset the packet counter
+      receiving_image = false;        // Reset the receiving image flag
+      receiving_credentials = false;  // Reset the receiving credentials flag
+    }
+    // Check if image data is being received
+    else if (receiving_image) {
+      // Copy the received packet data to the frame buffer
       if (image_size - packet_counter * PACKET_SIZE >= PACKET_SIZE) {
         for (int i = 0; i < PACKET_SIZE; i++) {
           fb[packet_counter * PACKET_SIZE + i] = buf[i];
@@ -88,94 +57,30 @@ void loop() {
           fb[(packet_counter)*PACKET_SIZE + i] = buf[i];
         }
       }
-      packet_counter++;
-      // Serial.printf("Packet counter: %d\n", packet_counter);
-    } else if (receiving_credentials) {
+      packet_counter++;  // Increment the packet counter
+    }
+    // Check if image credentials are being received
+    else if (receiving_credentials) {
+      // Get the image size from the received credentials
       image_size = got_image_credentials(buf);
       Serial.printf("Got credentials, the image size is: %d\n", image_size);
-      if (SET_DEBUG) { Serial.printf("Got image size of %d bytes\n", image_size); }
+      if (SET_DEBUG) {
+        Serial.printf("Got image size of %d bytes\n", image_size);
+      }
+      // Allocate memory for the image frame buffer
       fb = new uint8_t[image_size];
       if (fb == NULL) {
         Serial.println("Could not allocate memory");
         return;
       }
-      receiving_credentials = false;
-      receiving_image = true;
-    } else if (got_image_start(buf)) {
+      receiving_credentials = false;  // Reset the receiving credentials flag
+      receiving_image = true;         // Set the receiving image flag
+    }
+    // Check if the start of the image has been received
+    else if (got_image_start(buf)) {
       Serial.println("Got start");
-      receiving_credentials = true;
-      packet_counter = 0;
-    }
-    // the end
-  }
-}
-
-// ROUTINS
-void init_sd() {
-  Serial.println("Starting SD Card...");
-  // vspi->begin(VSPI_SCLK, VSPI_MISO, VSPI_MOSI, VSPI_SS);
-  if (!SD.begin(VSPI_SS, *vspi)) {
-    Serial.println("SD Card Mount Failed");
-    return;
-  } else {
-    Serial.println("SD card initialized successfully");
-  }
-  uint8_t cardType = SD.cardType();
-  if (cardType == CARD_NONE) {
-    Serial.println("No SD Card attached");
-    return;
-  }
-}
-
-bool got_image_start(uint8_t buffer[]) {
-  for (int i = 0; i < PACKET_SIZE; i++) {
-    if (buffer[i] != 0) {
-      return false;
+      receiving_credentials = true;  // Set the receiving credentials flag
+      packet_counter = 0;            // Reset the packet counter
     }
   }
-  return true;
-}
-
-int got_image_credentials(uint8_t buffer[]) {
-  // int image_size = (buffer[0] + buffer[1]) * PACKET_SIZE + buffer[2];
-
-  int full_packets = buffer[0] + (buffer[1] << 8);
-  int remainder = buffer[2];
-  int image_size = full_packets * PACKET_SIZE + remainder;
-
-  if (SET_DEBUG) { Serial.printf("The size of the received image: %d\n", image_size); }
-  return image_size;
-}
-
-bool got_image_end(uint8_t buffer[]) {
-  for (int i = 0; i < PACKET_SIZE; i++) {
-    if (buffer[i] != 1) {
-      return false;
-    }
-  }
-  return true;
-}
-
-void save_image_to_sd(uint8_t *fb, int size) {
-  Serial.println("Starting the writing to SD");
-
-  String path = "/picture" + String(image_counter) + ".jpg";
-
-  fs::FS &fs = SD;
-  Serial.printf("Picture file name: %s\n", path.c_str());
-
-  File file = fs.open(path.c_str(), FILE_WRITE);
-  if (!file) {
-    Serial.println("Failed to open file in writing mode");
-  } else {
-    Serial.printf("Size of frame buffer is : %d\n", size);
-    file.write(fb, size);
-    uint8_t n1 = 255;
-    uint8_t n2 = 217;
-    file.write(n1);
-    file.write(n2);
-    Serial.printf("Saved file to path: %s\n\n", path.c_str());
-  }
-  file.close();
-  image_counter++;
 }
--- current branch/ESP-CAM + nRF/RX/multiple_spi.h
+++ current branch/ESP-CAM + nRF/RX/multiple_spi.h
@@ -1,41 +1,22 @@
 #ifndef MULTIPLESPI_H
 #define MULTIPLESPI_H
 
-// // Pin definition for CAMERA_MODEL_AI_THINKER
-// #define PWDN_GPIO_NUM 32
-// #define RESET_GPIO_NUM -1
-// #define XCLK_GPIO_NUM 0
-// #define SIOD_GPIO_NUM 26
-// #define SIOC_GPIO_NUM 27
-// #define Y9_GPIO_NUM 35
-// #define Y8_GPIO_NUM 34
-// #define Y7_GPIO_NUM 39
-// #define Y6_GPIO_NUM 36
-// #define Y5_GPIO_NUM 21
-// #define Y4_GPIO_NUM 19
-// #define Y3_GPIO_NUM 18
-// #define Y2_GPIO_NUM 5
-// #define VSYNC_GPIO_NUM 25
-// #define HREF_GPIO_NUM 23
-// #define PCLK_GPIO_NUM 22
+#include <SPI.h>  // Include the SPI library
 
-#include <SPI.h>
+// Define pin mappings for VSPI (SPI2) interface
+#define VSPI_MISO 19  // MISO pin for VSPI
+#define VSPI_MOSI 23  // MOSI pin for VSPI
+#define VSPI_SCLK 18  // SCLK pin for VSPI
+#define VSPI_SS 5     // SS pin for VSPI
 
-// Define ALTERNATE_PINS to use non-standard GPIO pins for SPI bus
+// Define pin mappings for HSPI (SPI3) interface
+#define HSPI_MISO 12  // MISO pin for HSPI
+#define HSPI_MOSI 13  // MOSI pin for HSPI
+#define HSPI_SCLK 14  // SCLK pin for HSPI
+#define HSPI_SS 15    // SS pin for HSPI
 
-
-#define VSPI_MISO 19
-#define VSPI_MOSI 23
-#define VSPI_SCLK 18
-#define VSPI_SS 5
-
-#define HSPI_MISO 12
-#define HSPI_MOSI 13
-#define HSPI_SCLK 14
-#define HSPI_SS 15
-
-//uninitialised pointers to SPI objects
-SPIClass *vspi = NULL;
-SPIClass *hspi = NULL;
+// Declare pointers for SPIClass instances
+SPIClass *vspi = NULL;  // Pointer for VSPI instance
+SPIClass *hspi = NULL;  // Pointer for HSPI instance
 
 #endif /* MULTIPLESPI_H */
