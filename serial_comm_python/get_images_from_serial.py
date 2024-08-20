200176d67ec1087f9add1ace8fac91ce1653323a
S1rEBrum <85932316+S1rEBrum@users.noreply.github.com>
2024-08-20 10:54:36 +0300

    updated baud rate

--- serial_comm_python/get_images_from_serial.py
+++ serial_comm_python/get_images_from_serial.py
@@ -1,7 +1,7 @@
 import serial
 import time
 
-s = serial.Serial('/dev/cu.usbserial-0001', 115200, timeout=None)
+s = serial.Serial('/dev/cu.usbserial-0001', 460800, timeout=None)
 
 global counter
 counter = 0
@@ -12,7 +12,6 @@ while True:
     if s.inWaiting() > 0:
 
         with open(f"./images/image{counter}.jpg", "wb") as file:
-            start_time = time.time()  # Record the start time for processing
             counter += 1
             while True:
                 # Read and decode the byte to string
@@ -27,6 +26,8 @@ while True:
                             # Capture the relevant image data
                             image_data = word[26:]
                             word = ""  # Reset word to continue accumulating bytes
+                            start_time = time.time_ns()  # Record the start time for processing
+
 
                         # Check for the end condition
                         elif "Saved file" in word:
@@ -35,12 +36,14 @@ while True:
                                 byte_data = bytes(int(b)
                                                   for b in image_data.split())
                                 # Write the bytes to the file
+                                # print(image_data)
+                                # print(byte_data)
                                 file.write(byte_data)
                                 print(f"Finished writing image{counter-1}.jpg")
 
                                 # Calculate and print the elapsed time
-                                elapsed_time = time.time() - start_time
-                                print(f"Time taken for image{counter-1}.jpg: {elapsed_time:.2f} seconds")
+                                elapsed_time = time.time_ns() - start_time
+                                print(f"Time taken for image{counter-1}.jpg: {elapsed_time/1e6:.2f} ms")
                             except ValueError as e:
                                 print(f"Error converting image data: {e}")
 
@@ -51,6 +54,6 @@ while True:
                         elif image_data:
                             image_data += " " + word  # Continue accumulating the image bytes
 
-                        word = ""  # Reset the word for the next line
-
+                        word = ""  # Reset the word for the next line        
+        start_time = 0
     time.sleep(1)
